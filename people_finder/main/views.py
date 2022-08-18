6898d36373e9e61045b299bc5eaf99d4b86b9fb2
Azim Islam <19201026@uap-bd.edu>
2022-08-18 16:23:10 +0600

    Signup page works
    The signup page is now working

--- people_finder/main/views.py
+++ people_finder/main/views.py
@@ -9,9 +9,11 @@ def signup_request(request):
     if request.method == "POST":
         form = NewUserForm(request.POST)
         if form.is_valid():
+            print(form)
             user = form.save()
-            login()
-            return redirect("main:profile")
+            # login(request, user)
+            messages.success(request, "Signup successful." )
+            return redirect("main:signup")
         messages.error(request, "Invalid Informations!")
     form = NewUserForm()
     return render (request=request, template_name="main/signup.html", context={"register_form":form})
