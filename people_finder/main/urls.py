f851cb9af0263e6d992e51b7c5d07ba168d52b7d
Azim Islam <19201026@uap-bd.edu>
2022-08-25 01:04:10 +0600

    git gud

--- people_finder/main/urls.py
+++ people_finder/main/urls.py
@@ -5,10 +5,10 @@ app_name = "main"
 
 urlpatterns = [
     path("signup", views.signup_request, name="signup"),
+    path("", views.login_request, name="login"),
     path("login", views.login_request, name="login"),
     path("logout", views.logout_request, name="logout"),
     path("profile", views.profile_request, name="profile"),
     path("password_reset", views.password_reset_request, name="password_reset"),
     path("password_reset_profile", views.password_reset_profile_request, name="password_reset_profile"),
-
 ]
--- people_finder/people_finder/urls.py
+++ people_finder/people_finder/urls.py
@@ -21,8 +21,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('main.urls')),
+    path("", include('main.urls', namespace='mainapp')),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password/password_reset_done.html'), name='password_reset_done'), #email reset
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password/password_reset_complete.html'), name='password_reset_complete'), #final confirm not working      
-
 ]
