0f4df3e25c67a7f9aa53c6ca3d505d52c5b3a529
Shihab <19201051@uap-bd.edu>
2022-10-08 02:19:16 +0600

    search with first name

--- people_finder/main/views.py
+++ people_finder/main/views.py
@@ -219,6 +219,9 @@ def search_request(request):
     if request.method == "POST":
         searchV = request.POST["search_text"]
         searchValue = User.objects.filter(email__icontains=searchV)
+
+        if not(searchValue):
+            searchValue = Profile.objects.filter(first_name__icontains=searchV)
         
         tempObj = set()
         for user in searchValue:
