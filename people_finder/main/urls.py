fa66f4c27b1a4677cba30badd9b1899dc99db041
Busra Jahan Tanu <csbusrat@gmail.com>
2022-10-17 22:01:42 +0600

    Update urls

--- people_finder/main/urls.py
+++ people_finder/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from django.conf import settings 
+from django.conf import settings
 from django.conf.urls.static import static
+
 app_name = "main"
 
 urlpatterns = [
@@ -11,7 +12,11 @@ urlpatterns = [
     path("logout", views.logout_request, name="logout"),
     path("profile", views.profile_request, name="profile"),
     path("password_reset", views.password_reset_request, name="password_reset"),
-    path("password_reset_profile", views.password_reset_profile_request, name="password_reset_profile"),
+    path(
+        "password_reset_profile",
+        views.password_reset_profile_request,
+        name="password_reset_profile",
+    ),
     path("search", views.search_request, name="search"),
 ]
 if settings.DEBUG:  # new
