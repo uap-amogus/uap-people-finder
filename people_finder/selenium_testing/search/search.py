e69362fa90f54640950968cc795126728d532006
Azim Islam <19201026@uap-bd.edu>
2022-10-19 01:19:47 +0600

    Update search.py

--- people_finder/selenium_testing/search/search.py
+++ people_finder/selenium_testing/search/search.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Chrome()
 driver.get("http://103.198.137.87:8000/home/login")
+driver.maximize_window()
 time.sleep(2)
 
 username_field = driver.find_element(By.XPATH, '//*[@id="id_username"]')
@@ -45,25 +46,39 @@ try:
     search_inp = driver.find_element(By.XPATH, '//*[@id="id_search_text"]')
     search_inp.clear()
     search_inp.send_keys('19201026')
+    search_btn = driver.find_element(By.XPATH, '/html/body/div/div/div/form/button')
+    search_btn.click()
 except:
     print("Search page couldn't be loaded ❌")
 
 try:
-    assert '19201026@uap-bd.edu' in driver.page_source, "Search could not find the user (19201026@uap-bd.edu) ❌"
-    print("Search found the user (19201026@uap-bd.edu) ✅")
+    # assert 'Azim' in driver.page_source, "Search could not find the user (19201026@uap-bd.edu) ❌"
+    output_box = driver.find_elements(By.XPATH, './/td')
+    datas = [item.text for item in output_box]
+    assert "19201026@uap-bd.edu" in datas, 'Test: Search could not find the user (19201026@uap-bd.edu) ❌'
+    print("Test: Search found the user (19201026@uap-bd.edu) ✅")
 except AssertionError as msg:
     print(msg)
-
+time.sleep(2)
 #This case should fail
 try:
     search_inp = driver.find_element(By.XPATH, '//*[@id="id_search_text"]')
     search_inp.clear()
     search_inp.send_keys('111111')
-except:
+    search_btn = driver.find_element(By.XPATH, '/html/body/div/div/div/form/button')
+    search_btn.click()
+except Exception as msg:
     print("Search page couldn't be loaded ❌")
-
+    print(msg)
 try:
-    assert '111111@uap-bd.edu' in driver.page_source, "Search could not find the user (111111@uap-bd.edu) ❌"
-    print("Search found the user (111111@uap-bd.edu) ✅")
+    # assert 'Azim' in driver.page_source, "Search could not find the user (19201026@uap-bd.edu) ❌"
+    output_box = driver.find_elements(By.XPATH, './/td')
+    datas = [item.text for item in output_box]
+    assert "111111@uap-bd.edu" in datas, 'Test: Search could not find the user (111111@uap-bd.edu) ❌'
+    print("Test: Search found the user (19201026@uap-bd.edu) ✅")
 except AssertionError as msg:
     print(msg)
+
+time.sleep(1)
+driver.close()
+exit(0)
